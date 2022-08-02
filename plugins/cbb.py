5aab32743ee64e6d3b0e882533a91afcb20fcf52
SYTechieBots <109888747+SYTechieBots@users.noreply.github.com>
2022-08-02 07:04:46 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/GreyMatter658/advance-File-Sharing-Bot'>Click here</a>\n○ Channel : @GreyMatter_bots\n○ Support Group : @GreyMatters_bots_discussion</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/SYTechieBots/SY-File-Sharing-Bot'>Click here</a>\n○ Channel : @WoodPlex_OnlineStreaming\n○ Support Group : @WoodPlex_V1</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
